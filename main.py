485ad932b287f67d663f91ed729807b66f2efb80
chad_erasmus <chadhisairness@gmail.com>
2025-10-05 15:29:45 +0200

    player movement is working as expected

--- main.py
+++ main.py
@@ -3,20 +3,37 @@
 # throughout this file
 import pygame
 from constants import *
+import player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updateable, drawable)
+    
+    my_player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # my_player.draw(screen)
+        # my_player.update(dt)
+        updateable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
